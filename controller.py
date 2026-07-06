55991735be469f9aa3cec38658e683f87efce0d7
Kasper Dejke <kasperdejke@gmail.com>
2026-07-06 14:11:18 +0200

    restructured docker composition files

--- controller.py
+++ controller.py
@@ -5,12 +5,7 @@ targets = ["target", "database"]
 
 client = docker.from_env()
 attacker = client.containers.get("attacker")
-for target in targets:
-    print("nmap on ", target)
-    result = attacker.exec_run(f"nmap -sV {target}")
-    print(result.output.decode())
 
-print(f"fetching target/secret.txt")
 
 result = attacker.exec_run(f"curl http://target:8080/secret.txt")
 print(result.output.decode())
